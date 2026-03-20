764b61fee4b9d105c1b796d32a8e2e873fe45c47
Ndeye Oumy Ndiaye <ndeyeoumyndiaye80@gmail.com>
2026-03-20 18:54:42 +0000

    fix: server.ts cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,32 @@
 
-import app from './app'
-import {prisma} from './prismaClient'
+// import app from './app'
+// import {prisma} from './prismaClient'
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+
+// const start = async () => {
+//   try {
+//     // Test connexion Prisma
+//     await prisma.$connect();
+//     console.log('✅ Base de données connectée');
+
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Serveur PulseWay démarré sur le port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('❌ Erreur de connexion à la base de données', error);
+//     process.exit(1);
+//   }
+// };
+
+// start();
+import app from './app'
+import { prisma } from './prismaClient'
+
+const PORT = process.env.PORT || 3010;
 
 const start = async () => {
   try {
-    // Test connexion Prisma
     await prisma.$connect();
     console.log('✅ Base de données connectée');
 
